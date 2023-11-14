031e796f9b68c5d7b289b8b73466e5cb9d0cdd4a
Piotr Chabinka <chabinkapiotr@gmail.com>
2023-11-14 18:56:20 +0100

    Well... whole project

---
+++ pages/confirm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useRedirect()
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      Signing in...
+    </template>
+    Wait a moment while we sign you in
+  </UCard>
+</template>
+
+<style scoped>
+
+</style>
